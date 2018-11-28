f703796a07e8f911c6a30dbc5716b16a2e233f0c
floydnoel <floydnoel@users.noreply.github.com>
2018-11-27 17:02:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const errMsg = 'Bad input received, cannot convert'
 const promisify = require('util').promisify
 
 /**
-* Global callback definition (standard Node-style)
+* JSDoc callback definition, a standard Node-style callback expected by promisify.
 * @callback nodeCallback
 * @param {error} err - An error if one is encountered
 * @param {*} res - The successful result if processed
 */
 
 /**
- * Convert from an MS Excel-formatted date to a corresponding JS date object
+ * Convert from a spreadsheet-formatted date to a corresponding JS date object
  * @param {(number|string)} excelDate - An excel-formatted date number
  * @param {nodeCallback} [done] - A callback to use if desired
  * @returns {date}
@@ -59,12 +59,58 @@ const from = (excelDate, done) => {
   }
 }
 
-const to = (a, b) => 'a'
+/**
+ * Convert to a spreadsheet-formatted date from a corresponding JS date object
+ * @param {(number|string)} excelDate - An excel-formatted date number
+ * @param {nodeCallback} [done] - A callback to use if desired
+ * @returns {date}
+ * @throws Will throw an error if the input is invalid
+ */
+const to = (jsDate, done) => {
+  return 'foo'
+  
+  // ok...
+  // Check for a callback. If none, create our own to return the results
+  if (!done || typeof done !== 'function') {
+    done = (err, res) => {
+      if (err) throw err
+      return res
+    }
+  }
+  
+  // Check if the input exists
+  if (!excelDate && excelDate !== 0) {
+    return done(new Error(errMsg))
+  }
+
+  try {
+    // Check if the input is a JS Date
+    if (!(jsDate instanceof Date)) {
+      return done(new Error(errMsg))
+    } else {
+      // The input is indeed a JS Date
+      // For algorithm details see the README
+      const secondsInDay = 24 * 60 * 60
+      const excelEpoch = new Date(Date.UTC(1899, 11, 31))
+      const excelEpochAsUnixTimestamp = excelEpoch.getTime()
+      const missingLeapYearDay = secondsInDay * 1000
+      const delta = excelEpochAsUnixTimestamp - missingLeapYearDay
+      
+      const excelTimestampAsUnixTimestamp = jsDate.getTime() * secondsInDay * 1000
+      const parsed = excelTimestampAsUnixTimestamp + delta
+
+
+      return done(null, parsed)
+    }
+  } catch (e) {
+    return done(e)
+  }
+}
 
 module.exports = {
   from,
   to,
-  fromSync: promisify(from),
-  toSync: promisify(to),
+  fromAsync: promisify(from),
+  toAsync: promisify(to),
   errMsg,
 }
