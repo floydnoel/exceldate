b338a7fbfd4097e26d23bcc824b8d680a244e597
Floyd Noel <floydnoel@Floyds-MacBook-Pro-2.local>
2020-04-18 13:39:45 -0600

    updates and improvements

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # exceldate
 
-A node.js package to convert excel timestamps to JS dates. A simple utility function with no dependencies, exceldate accepts a number directly or stored in a string.
-
+A Node.js package to convert excel timestamps to JS dates. A simple utility function with no dependencies, exceldate accepts a number directly or stored in a string.
 
 ## Installation
 
 ```bash
-$ yarn add exceldate
-$ # or
-$ npm install exceldate
+yarn add exceldate
+# or
+npm install exceldate
 ```
-
 
 ## Usage
 
 Check it out:
+
 ```js
 const exceldate = require('exceldate')
 
@@ -31,21 +30,22 @@ exceldate(9000, (err, res) => {
 ## Testing
 
 Prerequisites:
+
 - clone/download the repo
-- install Jest e.g. `npm i -g jest`
-- navigate to the repo
+- navigate to the repo: `cd exceldate`
+- install dev dependencies to test: `yarn` or `npm i`
 
-Then run `yarn test` or `npm test`
-
+To test run `yarn test` or `npm test`
 
 ## License
 
 This is free and unencumbered software released "as is" into the public domain, without warranty of any kind.
 For more information, please refer to the LICENSE file and <https://unlicense.org>
 
-
 ## Changelog:
-- 0.0.3 First stable version
-- 1.0.0 support callback style interface, remove semicolons, updated test framework, updated README
-- 1.0.1 remove old code comment
-- 1.0.2 update README
+
+- 0.0.3: first stable version
+- 1.0.0: support callback style interface, remove semicolons, updated test framework, updated README
+- 1.0.1: remove old code comment
+- 1.0.2: update README
+- 1.1.0: added testing dev dependency, integrate eslint & prettier, added JSDoc types, friendlier error messages, invalid inputs will now throw an error, and README updates
--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 /**
- * Created by floyd noel on 1/30/17.
- *
- * TAKES AN EXCEL TIMESTAMP (AS A NUMBER OR A STRING), RETURNS A CORRESPONDING DATE OBJECT
+ * @callback nodeCallback
+ * @param {Error} [err]
+ * @param {Date} [res]
  */
-module.exports = (excelTimestamp, callback) => {
-  // ENSURE WE HAVE A VALID CALLBACK, OTHERWISE JUST RETURN
-  if (!callback || typeof callback !== 'function') {
-    callback = (err, res) => {
-      return !!err ? null : res
-    }
-  }
-  // ENSURE WE HAVE A TIMESTAMP
-  if (!excelTimestamp) {
-    return callback(new Error('No first argument provided, there is nothing to convert!'))
-  } else {
-    try {
-      // ENSURE TIMESTAMP IS A NUMBER, BASE 10
-      const timestampNumber = Number.parseFloat(excelTimestamp, 10)
-      if (Number.isNaN(timestampNumber)) {
-        return callback(new Error('First argument provided could not be used as a Number!'))
-      } else {
-        // APPARENTLY ALL GOOD, CONVERT THE EXCEL TIMESTAMP TO A DATE
-        // ALGORITHM DETAILS, AND CF COMMENTS: https://gist.github.com/christopherscott/2782634
-        const secondsInDay = 24 * 60 * 60
-        const excelEpoch = new Date(Date.UTC(1899, 11, 31))
-        const excelEpochAsUnixTimestamp = excelEpoch.getTime()
-        const missingLeapYearDay = secondsInDay * 1000
-        const delta = excelEpochAsUnixTimestamp - missingLeapYearDay
-        const excelTimestampAsUnixTimestamp = timestampNumber * secondsInDay * 1000
-        const parsed = excelTimestampAsUnixTimestamp + delta
-        const javascriptDate = new Date(parsed)
 
-        return callback(null, javascriptDate)
-      }
-    } catch (e) {
-      return callback(e)
+/**
+ * Takes an Excel timestamp (as a number or string) and returns a corresponding Date object
+ * @param {string|number} excelDate
+ * @param {nodeCallback} [done]
+ */
+module.exports = (
+  excelDate,
+  done = (err, res) => {
+    if (err) throw err
+    return res
+  }
+) => {
+  if (!excelDate) {
+    return done(new Error('No first argument provided, nothing to convert.'))
+  }
+  try {
+    // Convert input to a number
+    const excelDateNumber = Number.parseFloat(excelDate, 10)
+    if (Number.isNaN(excelDateNumber)) {
+      return done(new Error('First argument could not be parsed.'))
     }
+
+    // Convert input to JS Date
+    // Details here (mostly in comments): https://gist.github.com/christopherscott/2782634
+    const secondsInDay = 24 * 60 * 60
+    const excelEpoch = new Date(Date.UTC(1899, 11, 31))
+    const excelEpochTs = excelEpoch.getTime()
+    const missingLeapYearDay = secondsInDay * 1000
+
+    const excelDelta = excelEpochTs - missingLeapYearDay
+    const excelTs = excelDateNumber * secondsInDay * 1000
+    const unixTs = excelTs + excelDelta
+    const jsDate = new Date(unixTs)
+
+    return done(null, jsDate)
+  } catch (e) {
+    return done(e)
   }
 }
